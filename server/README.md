5a34ab2da446cb40b8a5f079f6e79441654e6069
cassiogroh <cassio_groh@hotmail.com>
2020-08-15 12:58:04 -0300

    Creating repo

---
+++ server/README.md
@@ -0,0 +1,8 @@
+# Server for my face recognition App
+<br/>
+
+* You can view the complete website working [here](https://cassiogroh.github.io/FaceRecognitionApp/); <br/>
+
+* I've used a query builder ([knex](http://knexjs.org/)) to run my [PostgreSQL](https://www.postgresql.org/) database; <br/>
+
+* More info about how this website works on my [front end](https://github.com/cassiogroh/FaceRecognitionAPI) repo built with [React](https://reactjs.org/);
---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const app = express();
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '123',
+        database: 'postgres'
+    }
+});
+
+// db.select('*').from('users');
+app.use(express.json()); // enabling req.body
+app.use(cors());
+
+app.get('/', (req, res) => {res.send(database.users)});
+app.post('/signin', signin.handleSignIn(db, bcrypt));
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+
+app.listen(3000, () => {
+    console.log(`app is ruinning on port 3000`);
+});
+
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => {
+//     console.log(`app is ruinning on port ${PORT}`);
+// });
+
+// PORT=3000 node server.js
